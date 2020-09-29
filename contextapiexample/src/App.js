b6b9d32a3c44d7db478cec58f7b11a1831f413f7
JAI <t.jaisaichand4@gmail.com>
2020-09-29 23:02:05 +0530

    create context

--- contextapiexample/src/App.js
+++ contextapiexample/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './App.css';
+import Firstchild from './Firstchild';
 
 function App() {
   return (
     <div className="App">
       <h1>Context api</h1>
+      <Firstchild></Firstchild>
     </div>
   );
 }
